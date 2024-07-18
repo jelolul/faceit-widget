12c3549e71fa55e7a8d9a2ba46649347911bfa42
jelo <jelo@bioxtorm.com>
2024-07-18 08:55:02 +0100

    add: social media cards

--- app/(no-layout)/widget/layout.jsx
+++ app/(no-layout)/widget/layout.jsx
@@ -6,6 +6,21 @@ export const metadata = {
     description: "Displays your FACEIT level and ELO on supported games in a customizable widget.",
     icons: {
         icon: "/assets/icons/favicon.svg"
+    },
+    openGraph: {
+        url: 'https://faceit-widget.vercel.app/',
+        title: "FACEIT Widget",
+        description: "Displays your FACEIT level and ELO on supported games in a customizable widget.",
+        type: 'website',
+        images: [
+            {
+                url: '/assets/images/faceit-widget-social.png',
+                secureUrl: '/assets/images/faceit-widget-social.png',
+                width: 1200,
+                height: 630,
+                alt: 'FACEIT Logo, next to it text that says "FACEIT Widget"',
+            }
+        ]
     }
 }
 
